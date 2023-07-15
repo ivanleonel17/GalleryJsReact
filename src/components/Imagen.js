f1c2db2e1fa5b4ce375998e7963466627e2abdcd
Ivan Antonuccio <ivan.leonnel@gmail.com>
2023-07-15 16:37:45 +0200

    Probando Props y puliendo css

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -1,13 +1,27 @@
 import React from "react";
 
-const Imagen = ({ photo }) => {
-	return (
-		<img
-			className="m-2 w-3/12 h-auto"
-			src={photo.urls.small}
-			alt={photo.alt_description}
-		/>
-	);
+const Imagen = (props) => {
+  return (
+    <article className="shadow-md bg-white rounded-3xl p-5">
+      <img
+        src={props.photo.urls.full}
+        alt={props.photo.alt_description}
+        loading="lazy"
+        className="h-52 w-full object-cover rounded-3xl md:h-80"
+      />
+
+      <div>
+        <p>Ubicacion fotografia: {props.photo.user.location}</p>
+        <p>Camara: {props.photo.exif && props.photo.exif.model}</p>
+        <p>Etiquetas:</p>
+        <ul>
+          {props.photo.tags.map((tag) => (
+            <li key={tag.title}>{tag.title}</li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  );
 };
 
 export default Imagen;
--- src/components/ListadoImagenes.js
+++ src/components/ListadoImagenes.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Imagen from "./Imagen";
 
-const ListadoImagenes = ({ photos }) => {
-	return (
-		<div className="container mt-4 flex flex-wrap">
-			{photos.map((photo) => (
-				<Imagen key={photo.id} photo={photo}>
-					Photo
-				</Imagen>
-			))}
-		</div>
-	);
+const ListadoImagenes = (props) => {
+  return (
+    <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 mt-5 ml-3">
+      {props.photos.map((photo) => (
+        <Imagen key={photo.id} photo={photo} />
+      ))}
+    </div>
+  );
 };
 
 export default ListadoImagenes;
